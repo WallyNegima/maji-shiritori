1792e3743d52ee490eee4e25fb8a7764ecd291d9
wally <u.str.gm@gmail.com>
2020-06-27 15:32:40 +0900

    create main component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="mainContainer">
+      <Main :mode="mode" />
+    </div>
     <Player
       v-for="user in $whim.users"
       :key="user.id"
@@ -14,19 +17,26 @@
 export default {
   name: "App",
   components: {
-    Player: () => import("@/components/Player/index")
+    Player: () => import("@/components/Player/index"),
+    Main: () => import("@/components/Main/index")
   },
   computed: {
     users() {
       return this.$whim.usrs;
+    },
+    mode() {
+      return "clockwise";
     }
   }
 };
 </script>
 
 <style scoped>
-.main {
-  position: relative;
-  z-index: 1;
+#mainContainer {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/Main/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="main">
+    <div v-if="mode == 'clockwise'">時計回り</div>
+    <div v-else-if="mode == 'counter-clockwise'">逆時計回り</div>
+    <div v-else-if="mode == 'bounus'">10秒ボーナス</div>
+    <div v-else-if="mode == 'onemore'">もう一度</div>
+    <div v-else-if="mode == 'more-5chars'">5文字以上</div>
+    <div v-else-if="mode == 'only-country'">国名縛り</div>
+    <div v-else-if="mode == 'good-voice'">野太い声で</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    mode: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+#main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  width: 18.056vw;
+  height: 12.014vw;
+  background: rgba(255, 255, 255, 0.7);
+  border-radius: 0.833vw;
+}
+</style>
